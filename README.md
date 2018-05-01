fd29e8260a76fc193ae23d67c4a4e3798295e188
jason-r-becker <jasonrichardbecker@gmail.com>
2018-04-30 22:27:06 -0700

    AR1 Model

---
+++ README.md
@@ -0,0 +1,5 @@
+# Empirical Utitlites
+
+This package include empircal methods funcitons including OLS and a matrix solver. The utilites module contains functions which cleanly print results to the console for easy interpretation and prints data structures into LaTeX formatting for easy conversion to reports.
+
+----
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='empiricalutilities',
+    version='0.0.1',
+    description='A python project for empirical methods',
+    author='Jason R Becker',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    download_url = 
+    keywords='empirical latex OLS',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests'])
+)
