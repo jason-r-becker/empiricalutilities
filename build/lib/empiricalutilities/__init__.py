fe11c2d629d7b0d9f88e23bcfc70874b271259a9
jason-r-becker <jasonrichardbecker@gmail.com>
2018-09-07 23:02:51 -0700

    replace_multiple

--- build/lib/empiricalutilities/__init__.py
+++ build/lib/empiricalutilities/__init__.py
@@ -1,3 +1,4 @@
 # __init__.py
-from .utilities import prettyPrint, add_start, correlation, color_table, latex_print, create_df
+from .utilities import prettyPrint, correlation, color_table, latex_print,
+from .utilities import create_df, dsub, datetime_range, add_start
 from .empiricalmethods import OLS, AR, ARCH_1, GARCH_11, sym_solve_matrix, t_test
--- empiricalutilities/__init__.py
+++ empiricalutilities/__init__.py
@@ -1,4 +1,5 @@
 # __init__.py
-from .utilities import prettyPrint, correlation, color_table, latex_print,
+from .utilities import prettyPrint, correlation, color_table, latex_print
 from .utilities import create_df, dsub, datetime_range, add_start
+from .utilities import replace_multiple, plot_explained_variance
 from .empiricalmethods import OLS, AR, ARCH_1, GARCH_11, sym_solve_matrix, t_test
