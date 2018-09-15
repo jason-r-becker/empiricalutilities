9bc9e6edaa847407652e3d32dfec5ce419480980
jason-r-becker <jasonrichardbecker@gmail.com>
2018-09-14 17:45:28 -0700

    fixed __init__.py

--- build/lib/empiricalutilities/__init__.py
+++ build/lib/empiricalutilities/__init__.py
@@ -1,7 +1,10 @@
 # __init__.py
-from .utilities import prettyPrint, correlation, color_table, latex_print
-from .utilities import date_subset, datetime_range, add_start
-from .utilities import replace_multiple, plot_explained_variance, custom_sort
-from .utilities import find_max_locs, combine_errors_table, remove_outer_periods
-from .utilities import find_min_locs
+from .utilities import prettyPrint, custom_sort, add_start, find_max_locs
+from .utilities import find_min_locs, combine_errors_table, replace_multiple,
+from .utilities import greeks_to_latex, latex_print, date_subset, datetime_range
+from .utilities import remove_outer_periods
+
 from .empiricalmethods import OLS, AR, ARCH_1, sym_solve_matrix, t_test
+
+from .visualizations import plot_color_table, plot_explained_variance
+from .visualizations import correlation, plot_ROC_curve, plot_confusion_matrix
